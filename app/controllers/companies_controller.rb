44238f06a559be9bd345cb2adfc7a03eb4b05b60
Rodrigo Cereceda <rcereced@gmail.com>
2016-08-29 19:39:25 -0300

    delete company using ajax

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -60,6 +60,7 @@ class CompaniesController < ApplicationController
     respond_to do |format|
       format.html { redirect_to companies_url, notice: 'Company was successfully destroyed.' }
       format.json { head :no_content }
+      format.js
     end
   end
 
