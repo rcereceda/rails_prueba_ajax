ccb6649b40da070e3eba967fe1e97144457851f8
Rodrigo Cereceda <rcereced@gmail.com>
2016-08-29 20:23:02 -0300

    seed for database

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -12,6 +12,8 @@ class CompaniesController < ApplicationController
   # GET /companies/1.json
   def show
     redirect_to user_session_path unless user_signed_in?
+    @claims = @company.claims
+    @claim = Claim.new
   end
 
   # GET /companies/new
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,21 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Claim.destroy_all
+User.destroy_all
+Company.destroy_all
+
+companies = []
+20.times do |i|
+	companies << Company.create(name: "Compañía #{i+1}")
+end
+
+users = []
+30.times do
+	users << User.create(email: "#{Faker::Name.first_name.downcase}@gmail.com", password: '123456')
+end
+
+250.times do |i|
+	Claim.create(comment: "Reclamo #{i+1}", user: users.sample, company: companies.sample, created_at: Faker::Date.between(30.days.ago, Date.today))
+end
